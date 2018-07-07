1ee31883c4e0eec9d6233edf09320d0340925a57
peterlxb <peterlxb@gmail.com>
2018-07-07 13:20:43 +0800

    Add mongodb

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("mongo conncted");
+  })
+  .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Server Running on port ${port}`));
